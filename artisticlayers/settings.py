2924819d4c8481628fe635c6e4e03f80fcd7b991
ParedesGab <paredesgab@gmail.com>
2025-04-05 15:36:03 +0200

    Set database configuration

--- artisticlayers/settings.py
+++ artisticlayers/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n4^d@)k848v3)y*60@9l@q2ev+-kww@h!8xp3gwksppsu&+3ln'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
