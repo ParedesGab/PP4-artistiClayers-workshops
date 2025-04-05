3ecc7879f8996356bb33a5e0198a1af751977aba
ParedesGab <paredesgab@gmail.com>
2025-04-05 12:14:58 +0200

    Install whitenoise-6.5.0 package

--- artisticlayers/settings.py
+++ artisticlayers/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n4^d@)k848v3)y*60@9l@q2ev+-kww@h!8xp3gwksppsu&+3ln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
