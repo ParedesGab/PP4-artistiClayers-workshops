4200b49edbf10fb57208ede30fa0a537ff4c54da
ParedesGab <paredesgab@gmail.com>
2025-04-09 14:52:05 +0200

    Fix: Correct __str__ for Workshop and rename level FK

--- workshop/admin.py
+++ workshop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Level, Workshop
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Level)
+class LevelAdmin(SummernoteModelAdmin):
+
+    list_display = ('name',)
+    search_fields = ['name']
+    list_filter = ('name',)
+
+
+@admin.register(Workshop)
+class WorkshopAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'is_public')
+    search_fields = ['level']
+    # list_filter = ('level',)
+    summernote_fields = ('description',)
--- workshop/models.py
+++ workshop/models.py
@@ -19,11 +19,13 @@ class Workshop(models.Model):
     Stores a single workshop entry.
     """
     level = models.ForeignKey(
-        Level, on_delete=models.CASCADE, related_name="workshop_levels"
+        Level, on_delete=models.CASCADE, related_name="workshops"
     )
+    name = models.CharField(max_length=50, blank=False, null=False,
+                            default="New Workshop")
     description = models.TextField()
     image = CloudinaryField('image', default='placeholder')
     is_public = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.level_id
+        return self.name
