aabac5953dee8674072faeef34a44dabb182ad88
ParedesGab <paredesgab@gmail.com>
2025-04-10 17:38:56 +0200

    Create model for collaboration app

--- collaboration/models.py
+++ collaboration/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CollaborateRequest(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.first_name} {self.last_name}"
