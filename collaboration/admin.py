baf565ca8b2d0f5c6824009fccf89ed3ae714f86
ParedesGab <paredesgab@gmail.com>
2025-04-10 17:50:43 +0200

    Register CollaborateRequest model in admin

--- collaboration/admin.py
+++ collaboration/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CollaborateRequest
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(SummernoteModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'read',)
+    summernote_fields = ('message',)
