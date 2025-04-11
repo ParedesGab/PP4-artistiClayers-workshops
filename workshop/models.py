e7c6867fe871117be19ac0d2e3eb5a1762e09f26
ParedesGab <paredesgab@gmail.com>
2025-04-11 13:50:26 +0200

    Create model Booking

--- workshop/models.py
+++ workshop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
@@ -29,3 +30,26 @@ class Workshop(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Booking(models.Model):
+    """
+    Stores a single Booking entry.
+    """
+    booked_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bookings"
+    )
+    workshop = models.ForeignKey(
+        Workshop, on_delete=models.CASCADE, related_name="workshop_bookings"
+    )
+    booking_date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["-booking_date"]
+
+    def __str__(self):
+        return f"""
+                {self.booked_by.username} booked '{self.workshop.name}'
+                on {self.booking_date.strftime('%Y-%m-%d %H:%M')}
+        """
--- workshop/urls.py
+++ workshop/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.WorkshopList.as_view(), name='workshops'),
-    path('<int:id>/', views.workshop_detail, name='workshop_detail'),
+    path('workshop/<int:id>/', views.workshop_detail, name='workshop_detail'),
 ]
