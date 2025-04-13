55d85817689287ac56636fc87d9974eed55b68b5
ParedesGab <paredesgab@gmail.com>
2025-04-13 12:18:13 +0200

    Display user bookings on booking page. For that booking_display view was updated to fetch the logged-in users bookings

--- workshop/views.py
+++ workshop/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Workshop
+from django.http import HttpResponseRedirect
+from .models import Workshop, Booking
 from .forms import BookingForm
-from django.urls import reverse
 
 
 class WorkshopList(generic.ListView):
@@ -44,6 +44,12 @@ def booking_display(request):
     """
     Displays the booking form and handles booking submissions.
     """
+    if request.user.is_authenticated:
+        user_bookings = Booking.objects.filter(
+            booked_by=request.user).order_by('-appointment_date')
+    else:
+        user_bookings = None
+
     if request.method == "POST":
         booking_form = BookingForm(data=request.POST)
         if booking_form.is_valid():
@@ -60,6 +66,40 @@ def booking_display(request):
         request,
         "workshop/booking.html",
         {
-            "booking_form": booking_form
+            "booking_form": booking_form,
+            'bookings': user_bookings,
         },
     )
+
+# This view returns you to the ? webpage after you've edited the comment
+# This return is done with a HttpResponseRedirect and reverse to refresh the post_detail view.
+
+
+def booking_edit(request, id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+        # queryset = Workshop.objects.filter(is_public=True)
+        # workshop = get_object_or_404(queryset, slug=slug)
+        booking = get_object_or_404(Booking, pk=id)
+        booking_form = BookingForm(data=request.POST, instance=booking)
+
+        if booking_form.is_valid() and booking.booked_by == request.user:
+            booking = booking_form.save(commit=False)
+            # comment.workshop = booking
+            booking.approved = False
+            booking.save()
+            messages.add_message(request, messages.SUCCESS, 'booking Updated!')
+        else:
+            messages.add_message(
+                request, messages.ERROR, 'Error updating booking!'
+                )
+
+    return render(
+        request,
+        "workshop/booking.html",
+        {"booking_form": booking_form},
+        )
+
+    # return HttpResponseRedirect(reverse('post_detail', args=[slug]))
