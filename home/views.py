b7cab0b7caa5a49c897f24897d5551592eeba0d2
ParedesGab <paredesgab@gmail.com>
2025-04-19 22:02:29 +0200

    Home app: check for any errors and missing doctstrings

--- home/views.py
+++ home/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 
 
 def home(request):
+    """
+    Renders the homepage of the website.
+    Displays the content defined in the 'home/index.html' template.
+
+    **Context**
+
+    This view does not pass any specific context variables to the template.
+
+    **Template**
+
+    :template:`home/index.html`
+    """
+
     template = "home/index.html"
     context = {}
     return render(request, template, context)
